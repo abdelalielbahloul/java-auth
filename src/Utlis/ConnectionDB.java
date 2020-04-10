b6d5f40329e855797c23ea436093b18512aed575
Abdelali EL BAHLOUL <bahloul97abdel@gmail.com>
2020-04-10 21:06:11 +0100

    design authentication interface and Connection class with jsbc provider

---
+++ src/Utlis/ConnectionDB.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utlis;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Abdelali-PC
+ */
+public class ConnectionDB {
+    private static Connection cnx;
+    static {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            cnx = DriverManager.getConnection("jdbc:mysql://localhost:3306/projet_ismc", "root","");
+        } catch (Exception ex) {
+            Logger.getLogger(ConnectionDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static Connection getCnx() {
+        return cnx;
+    }
+    
+}
